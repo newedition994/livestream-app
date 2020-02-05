feac98af0b103923e829bbb6e606e7211625a859
Breyanna Vass <breyannavass@gmail.com>
2020-02-05 15:40:50 -0500

    completed the ideaservice class and functions

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ class IdeaService {
   constructor() {
     this.ideas = [];
   }
+
+  async find() {
+    return this.ideas;
+  }
+
+  async create(data) {
+    const idea = {
+      id: this.ideas.length,
+      text: data.text,
+      tech: data.tech,
+      viewer: data.viewer
+    };
+
+    idea.time = moment().format("h:mm:ss a");
+
+    this.ideas.push(idea);
+
+    return idea;
+  }
 }
 
 const app = express(feathers());
