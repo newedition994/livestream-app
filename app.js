df71b7ee3fea1169d474624d8a57d27a8be96df7
Breyanna Vass <breyannavass@gmail.com>
2020-02-05 15:35:37 -0500

    making a port and a listening

--- app.js
+++ app.js
@@ -25,3 +25,11 @@ app.use("/ideas", new IdeaService());
 app.on("connection", conn => app.channel("stream").join(conn));
 // Publish events to stream
 app.publish(data => app.channel("stream"));
+
+const PORT = process.env.PORT || 3030;
+
+app
+  .listen(PORT)
+  .on("listening", () =>
+    console.log(`Realtime server running on port ${PORT}`)
+  );
